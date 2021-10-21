89ade646747b99e9badf853b4f053f49321d325b
Behnaz Nemati <behnaznemati2014@gmail.com>
2021-10-21 15:51:32 +0330

    feat: add auth custom errors

--- src/custom-errors/index.js
+++ src/custom-errors/index.js
@@ -1,6 +1,9 @@
 const InternalError = require('./InternalError');
 const RejectError = require('./RejectError');
 const RequestError = require('./RequestError');
+const AuthenticationError = require('./UnauthenticationError');
+const UnauthorizedError = require('./UnauthorizedError');
+const AccessDeniedError = require('./AccessDeniedError');
 const UserNotFoundError = require('./UserNotFoundError');
 const UserAlreadyExistError = require('./UserAlreadyExistError');
 const PasswordIncorrectError = require('./PasswordIncorrectError');
@@ -9,6 +12,9 @@ module.exports = {
   RejectError,
   InternalError,
   RequestError,
+  AuthenticationError,
+  UnauthorizedError,
+  AccessDeniedError,
   UserNotFoundError,
   UserAlreadyExistError,
   PasswordIncorrectError,
